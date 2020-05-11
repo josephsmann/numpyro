4def326fa4e11b185acd2d72f54d85ffe08eabca
josephsmann <mannalytics@gmail.com>
2020-05-11 11:22:12 -0600

    bug disappeared
    overnight - was getting identical samples, this morning I am not...

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,13 +3,57 @@ import janitor
 import altair as alt
 from numpyro.diagnostics import hpdi, print_summary
 
-def j_summary(samples):
-    print_summary(samples, 0.89, False)
-    df = pd.DataFrame(samples).clean_names()
+PD = pd.DataFrame
+def j_summary(samples, ctype='hist', properties={'width':800}):
+    if type(samples) == dict:
+        print_summary(samples, 0.89, False)
+        df = pd.DataFrame(samples).clean_names()
+    else:
+        print_summary(dict(zip(samples.columns, samples.T.values)), 0.89, False)
+        df = samples
     df = df if len(df)< 5000 else df.sample(n=4000)
-    base = alt.Chart(df).mark_bar().properties(height=30)
+    base = alt.Chart(df).properties(height=30)
 
-    return base.encode(
+    l = [ base.mark_line().transform_density(
+        row, as_ = [ row, 'density'], 
+        ).encode(
+        alt.X(f'{row}:Q'),
+        alt.Y('density:Q')
+    )  for row in df.columns]
+    
+    density = alt.vconcat(*l)
+
+        
+    
+    hist = base.mark_bar().encode(
         alt.X(bin=alt.Bin(maxbins=20), field=alt.repeat("row"), type='quantitative'),
         y = alt.Y(title=None, aggregate='count', type='quantitative')
     ).repeat(row=[c for c in df.columns])
+    
+    if ctype=='hist': return hist
+    else: return density
+   
+
+def alt_plot(domain, f, mark= 'line', properties={'width':800}):
+    df = pd.DataFrame({'x': domain, 'y': f(domain)})
+    c = alt.Chart(df).mark_line().encode(x='x:Q',y='y:Q').properties(**properties)
+    return c
+
+# def alt_density()
+
+# alt.Chart(source).transform_fold(
+#     ['petalWidth',
+#      'petalLength',
+#      'sepalWidth',
+#      'sepalLength'],
+#     as_ = ['Measurement_type', 'value']
+# ).transform_density(
+#     density='value',
+#     bandwidth=0.3,
+#     groupby=['Measurement_type'],
+#     extent= [0, 8]
+# ).mark_area().encode(
+#     alt.X('value:Q'),
+#     alt.Y('density:Q'),
+#     alt.Row('Measurement_type:N')
+# ).properties(width=300, height=50)
